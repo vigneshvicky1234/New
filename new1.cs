1a02b6e8d144d11b2bd3114550f30074f4d15f00
vigneshvicky1234 <127626606+vigneshvicky1234@users.noreply.github.com>
2023-03-17 01:47:45 +0530

    Add files via upload

---
+++ new1.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = new int[] { 1, 2, 3, 4, 5};
+for (int i = 0; i < arr.Length; i++)
+{
+    //for (int j = 0; j <i; j++)
+    //{
+    //Console.Write(' ');
+    {
+        Console.WriteLine(arr[i]);
+
+        {
+            Console.Write(" ");
+
+            
+               // Console.WriteLine("\t");
+            }
+        }
+
+    }
+
+
+
+
+
+
